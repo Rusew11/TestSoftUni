39653b531a54af2371353888551884bc806a093b
Rusew11 <Pavel.rusev11@gmail.com>
2025-11-18 20:27:39 +0200

    test.js update

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-//My new car test git example
-let cat - {
-  name 'Pavel',
+// My new car test git example
+let cat = {
+  name: 'Pavel',
   age: 8
 }
 
-
-//My new dog
-+let dog = {
-  + name: 'Rex',
-  + age: 1
-  
-+}
+// My new dog
+let dog = {
+  name: 'Rex',
+  age: 1
+}
