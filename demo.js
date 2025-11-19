5ec587738a7d05d1ad166cb2a80ca9775e8aeaad
Rusew11 <Pavel.rusev11@gmail.com>
2025-11-19 12:31:12 +0200

    first lesson js softuni fundamentals

---
+++ demo.js
@@ -0,0 +1,35 @@
+//let a = 5;
+
+//function demo() {
+    //function scope
+    //let b = 8;
+
+    //a++
+    //console.log(a);
+
+   // {
+  //      let c = 11;
+
+  //      console.log(c);
+  //  }
+
+  //  console.log(c);
+ //   console.log(b);
+//}
+
+   // console.log(a);
+
+    //demo();
+
+
+function demo() {
+    let n = 5;
+    
+    console.log(n);
+    
+    n++;
+
+    console.log(n);
+    }
+
+    demo();
