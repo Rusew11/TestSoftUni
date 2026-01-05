7078e0ada9fbf278114f1c1ba93650085c2f81cb
Rusew11 <Pavel.rusev11@gmail.com>
2026-01-05 15:02:40 +0200

    Update test.js
    firstPrepTask

--- exam_prep/test.js
+++ exam_prep/test.js
@@ -34,3 +34,14 @@ function computerStore(arr) {
         console.log(`Total price: ${priceWithTax.toFixed(2)}$`);
     }
 }
+
+computerStore([
+
+    '1050',
+    '200',
+    '450',
+    '2',
+    '18.50',
+    '16.86',
+    'special'
+]);
